27567eb659673715a25fa59f8fac192472f2479c
MMHLSZ <karencanataclofent@gmail.com>
2025-02-14 13:51:56 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -13,21 +13,26 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     /* For Login  Signup =========*/
-    document.addEventListener("already-account", function (event) {
+    document.addEventListener("click", function (event){
+      
       /* check if clicked element has the class 'nav-user' or 'already-account' */
-      if (event.target.closest(".nav-user, already-account")) {
+      if (event.target.closest(".nav-user, .already-account")) {
+        console.log(event.target.closest);
+        
         const forElement = document.querySelector(".form");
+        console.log(forElement);
+        
         forElement.classList.add("login-active");
         forElement.classList.remove("sign-up-active");
       }
-
+      
       /* check if clicked element has the class 'sign-up-btn' */
       if (event.target.closest(".sign-up-btn")) {
         const forElement = document.querySelector(".form");
         forElement.classList.remove("login-active");
         forElement.classList.add("sign-up-active");
       }
-
+      
       /* check if clicked element has the class 'form-cancel' */
       if (event.target.closest(".form-cancel")) {
         const forElement = document.querySelector(".form");
